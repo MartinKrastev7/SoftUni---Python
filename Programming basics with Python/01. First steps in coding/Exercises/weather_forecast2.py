2b225979314f646b6e331390dc0558e6395ae5b1
MartinKrastev7 <martin_krustev@abv.bg>
2022-04-12 19:40:10 +0300

    more exc - 10

---
+++ Programming basics with Python/01. First steps in coding/Exercises/weather_forecast2.py
@@ -0,0 +1,14 @@
+degree = float(input())
+
+if 26.00 <= degree <= 35.00:
+    print("Hot")
+elif 20.1 <= degree <= 25.9:
+    print("Warm")
+elif 15.00 <= degree <= 20.00:
+    print("Mild")
+elif 12.00 <= degree <= 14.9:
+    print("Cool")
+elif 5.00 <= degree <= 11.9:
+    print("Cold")
+elif degree < 5.00 or degree > 35:
+    print("unknown")
