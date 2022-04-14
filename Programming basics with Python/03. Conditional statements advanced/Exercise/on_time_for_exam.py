351797ac8527b8ca126db89569dd01fc31f5eee4
MartinKrastev7 <martin_krustev@abv.bg>
2022-04-14 18:58:53 +0300

    task 8

---
+++ Programming basics with Python/03. Conditional statements advanced/Exercise/on_time_for_exam.py
@@ -0,0 +1,33 @@
+hour_of_exam = int(input())
+minutes_of_exam = int(input())
+hour_of_arrival = int(input())
+minutes_of_arrival = int(input())
+time_of_exam = hour_of_exam * 60 + minutes_of_exam
+time_of_arrival = hour_of_arrival * 60 + minutes_of_arrival
+
+if time_of_arrival > time_of_exam:
+    print("Late")
+elif time_of_exam - 30 <= time_of_arrival <= time_of_exam:
+    print("On time")
+else:
+    print("Early")
+
+difference = abs(time_of_exam - time_of_arrival)
+if time_of_exam - 60 < time_of_arrival < time_of_exam:
+    print(f"{difference} minutes before the start")
+elif time_of_arrival <= time_of_exam - 60:
+    hours = difference // 60
+    minutes = difference % 60
+    if minutes < 10:
+        print(f"{hours}:0{minutes} hours before the start")
+    else:
+        print(f"{hours}:{minutes} hours before the start")
+elif time_of_exam < time_of_arrival < time_of_exam + 60:
+    print(f"{difference} minutes after the start")
+elif time_of_arrival >= time_of_exam + 60:
+    hours = difference // 60
+    minutes = difference % 60
+    if minutes < 10:
+        print(f"{hours}:0{minutes} hours after the start")
+    else:
+        print(f"{hours}:{minutes} hours after the start")
