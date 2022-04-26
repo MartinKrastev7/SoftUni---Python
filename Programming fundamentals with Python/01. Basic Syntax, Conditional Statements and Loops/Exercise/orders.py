784f0f92930392827b09be27af4e5e9d7f84c1dc
MartinKrastev7 <martin_krustev@abv.bg>
2022-04-26 18:27:45 +0300

    task 4

---
+++ Programming fundamentals with Python/01. Basic Syntax, Conditional Statements and Loops/Exercise/orders.py
@@ -0,0 +1,14 @@
+number_orders = int(input())
+
+total_price = 0
+
+for order in range(1, number_orders + 1):
+    price_per_capsule = float(input())
+    days = int(input())
+    capsules_count = int(input())
+
+    order_price = days * capsules_count * price_per_capsule
+    total_price += order_price
+    print(f"The price for the coffee is: ${order_price:.2f}")
+
+print(f"Total: ${total_price:.2f}")
