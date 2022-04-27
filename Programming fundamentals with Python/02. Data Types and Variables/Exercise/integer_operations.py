e92dfe6659daf6a79bbefd8571c95560ae837936
MartinKrastev7 <martin_krustev@abv.bg>
2022-04-27 18:36:44 +0300

    task 1

---
+++ Programming fundamentals with Python/02. Data Types and Variables/Exercise/integer_operations.py
@@ -0,0 +1,10 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+sum_of_first = n1 + n2
+divide_third = int(sum_of_first / n3)
+multiply_four = divide_third * n4
+
+print(multiply_four)
