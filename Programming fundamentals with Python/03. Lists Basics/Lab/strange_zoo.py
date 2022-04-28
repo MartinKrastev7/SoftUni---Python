aef1e13439dda05fc05855c939a368d7ec68ca3c
MartinKrastev7 <martin_krustev@abv.bg>
2022-04-28 13:47:46 +0300

    task 1

---
+++ Programming fundamentals with Python/03. Lists Basics/Lab/strange_zoo.py
@@ -0,0 +1,14 @@
+tail = input()
+body = input()
+head = input()
+
+list = [head, body, tail]
+print(list)
+
+# vtori nachin s razmqna na mesta - list = [tail, body, head] list[0],list[2]= list[2], list[0]
+
+#treti nachin
+list = []
+list.append(input())
+list.append(input())
+list.append(input())
