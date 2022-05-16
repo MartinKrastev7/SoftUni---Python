8f0c5d672a4a9dea7e325716cba2e2d9efd4325d
MartinKrastev7 <martin_krustev@abv.bg>
2022-05-16 21:41:25 +0300

    task 2

---
+++ Python advanced/01. Lists as Stacks and Queues/Lab/task2_matching_parentheses.py
@@ -0,0 +1,13 @@
+expression = input()
+s = []
+
+for i in range(len(expression)):
+
+    if expression[i] == "(":
+        s.append(i)
+    elif expression[i] == ")":
+        start_index = s.pop()
+        end_index = i
+        print(expression[start_index:end_index + 1])
+
+# is valid - stack should be empty at the end
