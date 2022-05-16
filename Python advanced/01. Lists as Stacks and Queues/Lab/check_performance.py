6865bd4aea10e352807fcb858f57a97930c1f042
MartinKrastev7 <martin_krustev@abv.bg>
2022-05-16 21:42:33 +0300

    check

---
+++ Python advanced/01. Lists as Stacks and Queues/Lab/check_performance.py
@@ -0,0 +1,23 @@
+import time
+
+s = []
+count = 2 ** 16
+result = 0
+
+start = time.time()
+for i in range(count):
+    s.append(i)
+while s:
+    result += s.pop()
+
+end = time.time()
+print(end - start)
+
+start = time.time()
+for i in range(count):
+    s.insert(0, i)
+while s:
+    result += s.pop(0)
+
+end = time.time()
+print(end - start)
