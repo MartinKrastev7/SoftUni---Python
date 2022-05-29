4527001656a79473226315844ef872ca1d4d321f
MartinKrastev7 <martin_krustev@abv.bg>
2022-05-29 12:52:43 +0300

    task 2

---
+++ Python advanced/03. Multidimensional Lists/Exercise/task2_diagonal_difference.py
@@ -0,0 +1,15 @@
+size = int(input())
+matrix = []
+for _ in range(size):
+    matrix.append([int(x) for x in input().split(" ")])
+
+primary = []
+secondary = []
+for idx in range(size):
+    primary.append(matrix[idx][idx])
+    secondary.append(matrix[idx][size - 1 - idx])
+
+primary_sum = sum(primary)
+secondary_sum = sum(secondary)
+result = abs(primary_sum - secondary_sum)
+print(result)
