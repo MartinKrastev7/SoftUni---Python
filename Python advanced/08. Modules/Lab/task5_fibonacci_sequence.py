c30962cf127d3b3c20cdb3b55d6166a163967da8
MartinKrastev7 <martin_krustev@abv.bg>
2022-06-29 21:07:09 +0300

    task 5

---
+++ Python advanced/08. Modules/Lab/task5_fibonacci_sequence.py
@@ -0,0 +1,15 @@
+from venv.create_sequence import create_sequence
+
+while True:
+    command = input().split()
+    if 'Create' in command:
+        num = int(command[-1])
+        sequence = create_sequence.sequence_creation(num)
+        sequence = (str(sequence).strip('[]').replace(', ', ' '))
+        print(sequence)
+
+    elif 'Locate' in command:
+        num = int(command[-1])
+        create_sequence.locate(num)
+    elif command == 'Stop':
+        break
