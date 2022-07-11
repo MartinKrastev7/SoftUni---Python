9a897f18bfecd297527b1d250804d6877d329e09
MartinKrastev7 <martin_krustev@abv.bg>
2022-07-11 18:28:38 +0300

    task 4

---
+++ Python OOP/03. Inheritance/Exercise/task4_need_for_speed/car.py
@@ -0,0 +1,8 @@
+from task4_need_for_speed.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    DEFAULT_FUEL_CONSUMPTION = 3
+
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
---
+++ Python OOP/03. Inheritance/Exercise/task4_need_for_speed/race_motorcycle.py
@@ -0,0 +1,8 @@
+from task4_need_for_speed.motorcycle import Motorcycle
+
+
+class RaceMotorcycle(Motorcycle):
+    DEFAULT_FUEL_CONSUMPTION = 8
+
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
