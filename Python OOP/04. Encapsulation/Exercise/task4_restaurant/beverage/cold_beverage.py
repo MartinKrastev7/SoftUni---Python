db790edee13310c5d7fea836d7d1123f61188aec
MartinKrastev7 <martin_krustev@abv.bg>
2022-07-11 18:33:06 +0300

    task 4

---
+++ Python OOP/04. Encapsulation/Exercise/task4_restaurant/beverage/cold_beverage.py
@@ -0,0 +1,6 @@
+from task4_restaurant.beverage.beverage import Beverage
+
+
+class ColdBeverage(Beverage):
+    def __init__(self, name, price, milliliters):
+        super().__init__(name, price, milliliters)
