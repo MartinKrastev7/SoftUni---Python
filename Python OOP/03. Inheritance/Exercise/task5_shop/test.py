af2506c60ca85babe18cc35b38cde1e34df97c16
MartinKrastev7 <martin_krustev@abv.bg>
2022-07-11 18:28:56 +0300

    task 5

---
+++ Python OOP/03. Inheritance/Exercise/task5_shop/test.py
@@ -0,0 +1,13 @@
+from task5_shop.drink import Drink
+from task5_shop.food import Food
+from task5_shop.product_repository import ProductRepository
+
+food = Food("apple")
+drink = Drink("water")
+repo = ProductRepository()
+repo.add(food)
+repo.add(drink)
+print(repo.products)
+print(repo.find("water"))
+repo.find("apple").decrease(5)
+print(repo)
