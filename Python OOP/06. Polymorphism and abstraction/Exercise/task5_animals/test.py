e632a755dd91b891de7eb686f5ad6aee4dd60351
MartinKrastev7 <martin_krustev@abv.bg>
2022-07-16 17:56:48 +0300

    task 5

---
+++ Python OOP/06. Polymorphism and abstraction/Exercise/task5_animals/test.py
@@ -0,0 +1,9 @@
+from task5_animals.dog import Dog
+from task5_animals.tomcat import Tomcat
+
+dog = Dog("Rocky", 3, "Male")
+print(dog.make_sound())
+print(dog)
+tomcat = Tomcat("Tom", 6)
+print(tomcat.make_sound())
+print(tomcat)
