073f51f54627fdf23e14a3e221bc0eadc690b9dc
MartinKrastev7 <martin_krustev@abv.bg>
2022-07-16 17:56:31 +0300

    task 4

---
+++ Python OOP/06. Polymorphism and abstraction/Exercise/task4_wild_farm/test.py
@@ -0,0 +1,13 @@
+from task4_wild_farm.animals.birds import Hen
+from task4_wild_farm.food import Vegetable, Fruit, Meat
+
+hen = Hen("Harry", 10, 10)
+veg = Vegetable(3)
+fruit = Fruit(5)
+meat = Meat(1)
+print(hen)
+print(hen.make_sound())
+hen.feed(veg)
+hen.feed(fruit)
+hen.feed(meat)
+print(hen)
